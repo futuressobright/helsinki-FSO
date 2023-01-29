4176669c4f36db3310e61e09662a32913636bf38
Ashish Mukharji <ashish@ashish.org>
2023-01-29 11:19:14 -0800

    moved components out of App into their own block

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,3 +1,32 @@
+const Header = (props) => {
+	console.log(props)
+	return (
+		<div>
+			<h1>{props.course}</h1>
+		</div>
+	)
+
+}
+
+const Content = (props) => {
+	console.log(props)
+	return (
+		<div>
+			<p>{props.part}</p>
+		</div>
+	)
+}
+
+const Total = (props) => {
+	console.log(props)
+	return (
+		<div>
+			<p>{props.exercise}</p>
+		</div>
+	)
+}
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -7,34 +36,6 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
-
-  const Header = (props) => {
-	console.log(props)
-	return (
-		<div>
-			<h1>{props.course}</h1>
-		</div>
-	)
-
-  }
-
-  const Content = (props) => {
-	console.log(props)
-	return (
-		<div>
-			<p>{props.part}</p>
-		</div>
-	)
-  }
-
-  const Total = (props) => {
-	console.log(props)
-	return (
-		<div>
-			<p>{props.exercise}</p>
-		</div>
-	)
-  }
 
 
   return (
