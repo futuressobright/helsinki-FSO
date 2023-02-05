444d402ab25a7dcb04485db7eb237b6ca0201a08
Ashish Mukharji <ashish@ashish.org>
2023-02-04 22:00:32 -0800

    1.6 unicafe step1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,69 +1,45 @@
-// 1.5 single javascript object
-const Header = (props) => {
-	return (
-		<div>
-			<h1>{props.course.name}</h1>
-		</div>
-	)
-
-}
-
-
-const Content = (props) => {
-	return (
-		<div>
-			<Part part={props.parts[0]}/>
-			<Part part={props.parts[1]}/>
-			<Part part={props.parts[2]}/>
-		</div>
-	)
-}
-
-
-const Part = (props) => {
-        return (
-                <div>
-			<p>{props.part.name} {props.part.exercises} </p>
-                </div>
-        )
-
-}
-
-
-const Total = (props) => {
-	return (
-		<div>
-			<p>{props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-		</div>
-	)
-
-}
-
+// 1.6 unicafe step1
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
   }
 
   return (
     <div>
-	<Header   course={course} />
-	<Content  parts={course.parts} />
-	<Total    parts={course.parts} />
+
+      <h1>Geben sie feedback, bitte!</h1>
+
+      <button onClick={handleGoodClick}>
+        good
+      </button>
+
+      <button onClick={handleNeutralClick}>
+        neutral
+      </button>
+
+      <button onClick={handleBadClick}>
+        bad
+      </button>
+
+      <h1>Achtung, hier sind Statistiks!</h1>
+      <p>Good:  {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad:  {bad}</p>
     </div>
   )
 }
